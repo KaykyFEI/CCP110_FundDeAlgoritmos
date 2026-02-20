eb9d3f2d7ce484af47d5f1e8313a68279a9248c2
KaykyFEI <feispkpinheiro@fei.edu.br>
2026-02-20 16:25:32 -0300

    Remove exercicios para branch dedicada

--- exercicio2.py
+++
@@ -1,8 +0,0 @@
-kmPercorridos = float(input("Digite a quantidade de quilômetros percorridos: "))
-diasAlugado = float(input("Digite a quantidade de dias que o carro foi alugado: "))
-
-custoAluguel = diasAlugado*60.00
-custoKmPercorridos = kmPercorridos*0.15
-custoTotal = custoAluguel + custoKmPercorridos
-
-print(custoTotal)
--- exercicio3.py
+++
@@ -1,8 +0,0 @@
-dias = int(input("Digite uma quantidade de dias: "))
-horas = int(input("Digite uma quantidade de horas: "))
-minutos = int(input("Digite uma quantidade de minutos: "))
-segundos = int(input("Digite uma quantidade de segundos: "))
-
-tempoTotal = (dias*86400) + (horas*3600) + (minutos*60) + (segundos*1)
-
-print("O tempo total em segundos foi de %d segundos" % tempoTotal)
