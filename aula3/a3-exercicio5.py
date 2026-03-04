0d68a76588e1047b83bc021ae484da7a792891a8
KaykyFEI <feispkpinheiro@fei.edu.br>
2026-03-04 00:25:14 -0300

    finalização do exercício 5.

--- aula3/a3-exercicio5.py
+++ aula3/a3-exercicio5.py
@@ -1,23 +1,34 @@
 
 pontuacao = 0
 
-ligacao = input("Telefonou para a vítima? (S(im) / N(ão))")
-local = input("Esteve no local do crime? S(im) / N(ão)")
-distancia = input("Mora perto da vítima? S(im) / N(ão)")
-divida = input("Devia para a vítmia? S(im) / N(ão)")
-trabalho = input("Já trabalhou com a vítima? S(im) / N(ão)")
+ligacao = input("Telefonou para a vítima? (S(im) / N(ão)): ")
+local = input("Esteve no local do crime? S(im) / N(ão): ")
+distancia = input("Mora perto da vítima? S(im) / N(ão): ")
+divida = input("Devia para a vítima? S(im) / N(ão): ")
+trabalho = input("Já trabalhou com a vítima? S(im) / N(ão): ")
 
 if ligacao == "S" or ligacao =="Sim" or ligacao == "s" or ligacao == "sim":
     pontuacao = pontuacao + 1
-else:
-    pontuacao = pontuacao - 1
 
 if local == "S" or local == "Sim" or local == "s" or local == "sim":
     pontuacao = pontuacao + 1
-else:
-    pontuacao = pontuacao - 1
 
 if distancia == "S" or distancia == "Sim" or distancia == "s" or distancia == "sim":
     pontuacao = pontuacao + 1
-else:
+
+if divida == "S" or  divida == "Sim" or divida == "s" or divida == "sim":
     pontuacao = pontuacao + 1
+
+if trabalho == "S" or trabalho == "Sim" or trabalho == "s":
+    pontuacao = pontuacao + 1
+
+if pontuacao == 5:
+    classificacao = "Assassino"
+elif pontuacao == 3 or pontuacao == 4:
+    classificacao = "Cúmplice"
+elif pontuacao == 2:
+    classificacao = "Suspeita"
+else:
+    classificacao = "Inocente"
+
+print(classificacao)
